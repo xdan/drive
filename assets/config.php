2369664a91e3f082cb3fff30d6e3db45c46b17b8
Valeriy <skoder@ya.ru>
2014-12-05 12:37:27 +0500

    added user modul

--- assets/config.php
+++ assets/config.php
@@ -1,19 +1,5 @@
 <?php
 return array(
-	'users' => array(
-		array(
-			'login'=>'demo',
-			'password'=>'demo',
-			'virtual_root' => ROOT.'tmp'.DS.'demo1'.DS,
-			'salt'=> 'ndcgh;l2;lga',
-		),
-		array(
-			'login'=>'demo2',
-			'password'=>'demo',
-			'virtual_root' => ROOT.'tmp'.DS.'demo2'.DS,
-			'salt'=> 'ndcgh;l2;lga',
-		),
-	),
 	'virtual_root' => ROOT.'tmp'.DS,
 	'time_format' => 'H:i:s d/m/Y',
 	'white_extensions' => array(),
---
+++ assets/config.users.php~
@@ -0,0 +1,16 @@
+<?php
+return array(
+	array(
+		'login'=>'demo',
+		'password'=>'demo',
+		'virtual_root' => 'demo1'.DS,
+		'salt'=> 'ndcgh;l2;lga',
+		'role'=>'admin'
+	),
+	array(
+		'login'=>'demo2',
+		'password'=>'demo',
+		'virtual_root' => 'demo2'.DS,
+		'salt'=> 'ndcgh;l2;lga',
+	),
+);
